000532288b717b13d6b107d42d78b97986541898
Tomasz Abramski <tomek.abramski@gmail.com>
2016-07-07 17:04:46 +0200

    f

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var https = require('https');
 var app = express();
+var fs = require('fs');
 var bodyParser = require('body-parser');
-
+var sslkey = fs.readFileSync('ssl-key.pem');
+var sslcert = fs.readFileSync('ssl-cert.pem');
+var httpsOptions = {key: sslkey, cert: sslcert};
 
 app.use('/', express.static('public'));
-app.listen(process.env.PORT || 3000, function() {
-  console.log('Example app listening on port 3000!');
+
+
+https.createServer(httpsOptions, app).listen(8000, function() {
+    console.log('Express HTTPS server listening on port 8000');
 });
